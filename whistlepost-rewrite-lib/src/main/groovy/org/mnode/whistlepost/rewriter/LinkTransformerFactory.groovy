d0af9dd0b1aae1fc0852d86dc3b010d086e3bd94
Ben Fortuna <bfortuna@odecee.com.au>
2016-07-07 11:11:41 +1000

    Update default rewriter config

--- whistlepost-rewrite-lib/src/main/groovy/org/mnode/whistlepost/rewriter/LinkTransformerFactory.groovy
+++ whistlepost-rewrite-lib/src/main/groovy/org/mnode/whistlepost/rewriter/LinkTransformerFactory.groovy
@@ -18,10 +18,10 @@ class LinkTransformerFactory extends RegexLinkTransformer implements Transformer
     @Property(value = 'wp-link-rewriter', propertyPrivate = true)
     private static final String PIPELINE_TYPE = 'pipeline.type';
 
-    @Property(label = 'Link Pattern', description = 'Regular expression for matching links', value='^/content(/wp/.*)$')
+    @Property(label = 'Link Pattern', description = 'Regular expression for matching links', value='^/content(/wp)?(/\\w+)?(/.*)$')
     private static final String LINK_PATTERN = "pattern.link";
 
-    @Property(label = "Replacement link", description = "Replacement string for links", value='$1')
+    @Property(label = "Replacement link", description = "Replacement string for links", value='$1$3')
     private static final String REPLACEMENT_STRING = "replacement.string";
 
     @Activate
