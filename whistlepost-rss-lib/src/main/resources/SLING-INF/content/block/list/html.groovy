4f2c2fa11abe81ce0dd8a0b3bf2ae4c0e075be52
Ben Fortuna <bfortuna@odecee.com.au>
2016-08-06 00:33:46 +1000

    Added basic support for rss feeds with limit

--- whistlepost-rss-lib/src/main/resources/SLING-INF/content/block/list/html.groovy
+++ whistlepost-rss-lib/src/main/resources/SLING-INF/content/block/list/html.groovy
@@ -4,10 +4,14 @@ out.write """
 """
 
 def rssFeed = currentNode.getProperty('feedUrl').string.toURL().text
+def limit = request.requestPathInfo.suffix ? request.requestPathInfo.suffix.substring(1) as int : Integer.MAX_VALUE;
 
 def rss = new XmlSlurper().parseText(rssFeed)
-rss.channel.item.each{
-    out.write("<li><a href=\"$it.link\">$it.title</a></li>")
+rss.channel.item.list().take(limit).each {
+    out.write("<li><a href=\"$it.link\" title=\"${it.description.text()}\">$it.title</a></li>")
+}
+if (currentNode.hasProperty('htmlUrl')) {
+    out.write("<li><a href=\"${currentNode.getProperty('htmlUrl').string}\">More..</a></li>")
 }
 
 out.write """
