2bb29aaea4c209a70ca55abea66eed4c4552bd49
Ben Fortuna <benfortuna@gmail.com>
2016-10-28 03:38:37 +0000

    Updated bundle plugin

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 : ${SLING_HOST?"Need to set SLING_HOST variable"}
 : ${SLING_PORT?"Need to set SLING_PORT variable"}
-./gradlew build deployBundle -Psling_host=$SLING_HOST -Psling_port=$SLING_PORT
+./gradlew build bundleInstall -Psling_host=$SLING_HOST -Psling_port=$SLING_PORT
--- site-starter/templates/whistlepost-site/scripts/install.sh
+++ site-starter/templates/whistlepost-site/scripts/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 : ${SLING_URL?"Need to set SLING_URL variable"}
-./gradlew build installBundle -Psling_url=$SLING_URL
+./gradlew build bundleInstall -Psling_url=$SLING_URL
