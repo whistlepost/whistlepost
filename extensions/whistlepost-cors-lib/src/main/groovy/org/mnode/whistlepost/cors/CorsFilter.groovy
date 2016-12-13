78d3ef42711ed6911575cf95f4bcc147a7a11094
Ben Fortuna <benfortuna@gmail.com>
2016-12-13 00:48:01 +0000

    Added CORS filter

---
+++ extensions/whistlepost-cors-lib/src/main/groovy/org/mnode/whistlepost/cors/CorsFilter.groovy
@@ -0,0 +1,52 @@
+package org.mnode.whistlepost.cors
+
+import java.util.regex.Pattern
+
+import javax.servlet.Filter
+import javax.servlet.FilterConfig
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.ServletException
+
+import org.apache.felix.scr.annotations.Activate
+import org.apache.felix.scr.annotations.Component
+import org.apache.felix.scr.annotations.Properties
+import org.apache.felix.scr.annotations.Property
+import org.apache.felix.scr.annotations.Service
+import org.osgi.service.component.ComponentContext
+
+@Component(immediate = true, metatype = false, label = 'Whistlepost CORS Filter', description = 'Applies CORS headers where applicable')
+@Service(value = Filter.class)
+@Property(name = 'service.description', value = 'Whistlepost CORS Filter')
+@Property(name = "service.vendor", value = "Micronode")
+@Property(name = "sling.filter.scope", value = "request")
+class CorsFilter implements Filter {
+
+    @Property(label = 'Origin Pattern', description = 'Regular expression for matching request origin', value = /^.*\.example\.com$/)
+    private static final String ORIGIN_PATTERN = "pattern.origin";
+    
+    Pattern originPattern
+
+    @Activate
+    protected void activate(ComponentContext ctx) {
+        originPattern =~ ctx.properties[ORIGIN_PATTERN]
+    }
+    
+    void init(FilterConfig config) {}
+    
+    void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        String origin = request.getHeader("Origin");
+        if (origin ==~ originPattern) {
+            response.addHeader("Access-Control-Allow-Origin", origin);
+            response.addHeader("Access-Control-Allow-Methods", 'GET, POST, OPTIONS')
+        }
+        chain.doFilter(request, response)
+    }
+    
+    void destroy() {}
+}
