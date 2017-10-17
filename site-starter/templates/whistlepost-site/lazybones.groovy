a3ab9df4aa8ce041dbfcbf8db2ae2b2c43d8115a
Ben Fortuna <bfortuna@odecee.com.au>
2017-10-18 10:03:44 +1100

    Updated site template to support content tokenization

--- site-starter/templates/whistlepost-site/lazybones.groovy
+++ site-starter/templates/whistlepost-site/lazybones.groovy
@@ -1,7 +1,11 @@
 def props = [:]
-props.group = ask("Define value for 'group' [org.example]: ", "org.example", "group")
+props.organization = ask("Define value for 'organization' [My Org]: ", "My Org", "organization")
+def defaultGroup = "org.${props.organization.toLowerCase().split().join('')}"
+props.group = ask("Define value for 'group' [$defaultGroup]: ", $defaultGroup, "group")
 props.version = ask("Define value for 'version' [0.1]: ", "0.1", "version")
 props.projectName = ask("Define value for 'projectName' [My Site]: ", "My Site", "projectName")
+def defaultProjectUrl = "http://${props.projectName.toLowerCase().split().join('.')}"
+props.projectUrl = ask("Define value for 'projectUrl' [$defaultProjectUrl]: ", defaultProjectUrl, "projectUrl")
 def defaultProjectId = props.projectName.toLowerCase().split().join('-')
 props.projectId = ask("Define value for 'projectId' [$defaultProjectId]: ", defaultProjectId, "projectId")
 
