3fa462f65089b505fcef241563c7ebf320d76671
Ben Fortuna <benfortuna@gmail.com>
2018-03-30 00:05:38 +1100

    Added editors

--- whistlepost-app/src/main/resources/SLING-INF/content/js/whistlepost.js
+++ whistlepost-app/src/main/resources/SLING-INF/content/js/whistlepost.js
@@ -5,3 +5,11 @@ import '../css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import 'html5shiv';
 //import 'respond';
+
+// forms..
+function parseTextarea(form_id, val, input_field) {
+    var pars = val.split('\n')
+    for (var i in pars) {
+        $('#' + form_id).append('<input type="hidden" name="' + input_field + '" value="' + pars[i] + '" />');
+    }
+}
