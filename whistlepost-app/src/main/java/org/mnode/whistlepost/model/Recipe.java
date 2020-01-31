fc0df0ef30783b5de1d128941b194acf90d24795
Ben Fortuna <bfortuna@odecee.com.au>
2020-02-01 10:37:37 +1100

    Added recipe model

--- whistlepost-app/src/main/java/org/mnode/whistlepost/model/Recipe.java
+++ whistlepost-app/src/main/java/org/mnode/whistlepost/model/Recipe.java
@@ -1,4 +1,26 @@
 package org.mnode.whistlepost.model;
 
-public class Recipe {
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class})
+public class Recipe extends Article {
+
+    public String[] getIngredients() {
+        return resource.getValueMap().get("ingredients", new String[] {});
+    }
+
+    public String[] getMethod() {
+        return resource.getValueMap().get("method", new String[] {});
+    }
+
+    public Recipe getSidebar() {
+        String sidebar = resource.getValueMap().get("sidebar", String.class);
+        if (sidebar != null) {
+            return resourceResolver.getResource(resource.getParent(), sidebar).adaptTo(Recipe.class);
+        } else {
+            return null;
+        }
+    }
 }
