7f4c216b9996393dfb5390367a669db8431a6b17
Ben Fortuna <bfortuna@odecee.com.au>
2020-02-01 10:36:19 +1100

    Support sidebar and related articles

--- whistlepost-app/src/main/java/org/mnode/whistlepost/model/Article.java
+++ whistlepost-app/src/main/java/org/mnode/whistlepost/model/Article.java
@@ -2,10 +2,18 @@ package org.mnode.whistlepost.model;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Model(adaptables = {Resource.class, SlingHttpServletRequest.class})
 public class Article extends Page {
+
+    @SlingObject
+    protected ResourceResolver resourceResolver;
 
     public String getSummary() {
         if (getPars().length > 0) {
@@ -26,5 +34,31 @@ public class Article extends Page {
 
     public String getMedia() {
         return resource.getValueMap().get("media", String.class);
+    }
+
+    public Article getSidebar() {
+        Resource sidebarResource = null;
+
+        String sidebar = resource.getValueMap().get("sidebar", String.class);
+        if (sidebar != null) {
+            if (sidebar.startsWith("/")) {
+                sidebarResource = resourceResolver.getResource(sidebar);
+            } else {
+                sidebarResource = resourceResolver.getResource(resource.getParent(), sidebar);
+            }
+        }
+
+        if (sidebarResource != null) {
+            return sidebarResource.adaptTo(Article.class);
+        } else {
+            return null;
+        }
+    }
+
+    public Iterable<Article> getRelated() {
+        String[] related = resource.getValueMap().get("related", new String[] {});
+
+        return Arrays.stream(related).map(p -> resourceResolver.getResource(resource.getParent(), p).adaptTo(Article.class))
+                .collect(Collectors.toList());
     }
 }
