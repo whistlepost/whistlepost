04949d01c507190360f02a25eb1b809a308b6925
Ben Fortuna <bfortuna@odecee.com.au>
2020-04-03 16:52:56 +1100

    Fixed deploy script

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -3,4 +3,5 @@
 #git tag -f release/$RELEASE_NAME && git push origin :refs/tags/release/$RELEASE_NAME && git push origin refs/tags/release/$RELEASE_NAME
 
 # Tag release and upload artifacts to bintray..
-./gradlew -q -Prelease.customUsername=$GIT_USER -Prelease.customPassword=$GIT_PASSWORD -Prelease.disableChecks -Prelease.pushTagsOnly release && ./gradlew bintrayUpload
+./gradlew -q -Prelease.customUsername=$GIT_USER -Prelease.customPassword=$GIT_PASSWORD -Prelease.disableChecks -Prelease.pushTagsOnly release \
+  && ./gradlew build bintrayUpload
