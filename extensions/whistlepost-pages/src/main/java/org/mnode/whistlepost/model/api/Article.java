bdf77f1686ff31510735c341b556983b643a95da
Ben Fortuna <bfortuna@odecee.com.au>
2020-08-30 10:31:59 +1000

    Provide model api as basis for page models

---
+++ extensions/whistlepost-pages/src/main/java/org/mnode/whistlepost/model/api/Article.java
@@ -0,0 +1,33 @@
+package org.mnode.whistlepost.model.api;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.ResourcePath;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * An article represents content that can be rendered as a published media-rich post.
+ * An article extends from a page to add a mandatory abstract, article body, published date,
+ * optional list of media, an optional sidebar article, and optional related articles.
+ */
+@Model(adaptables = {Resource.class})
+public interface Article extends Page {
+
+    @Inject @Optional
+    String getAbstract();
+
+    @Inject @Optional
+    BlockResource getBody();
+
+    @Inject @Optional
+    Iterable<String> getMedia();
+
+    @ResourcePath(name = "sidebar") @Optional
+    Article getSidebar();
+
+    @Inject @Optional
+    List<Reference> getRelated();
+}
---
+++ extensions/whistlepost-pages/src/main/java/org/mnode/whistlepost/model/api/Page.java
@@ -0,0 +1,36 @@
+package org.mnode.whistlepost.model.api;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+
+/**
+ * A page represents any content that can exist as a discrete, independent resource.
+ */
+@Model(adaptables = {Resource.class})
+public interface Page {
+
+    @Self
+    Metadata getMetadata();
+
+    @Self
+    Resource getResource();
+
+    /**
+     * Returns a list of all parent pages in the content hierarchy.
+     * @return an iterable list of pages
+     */
+    Iterable<Page> getParents();
+
+    /**
+     * Returns a list of all sibling pages in the content hierarchy.
+     * @return an iterable list of pages
+     */
+    Iterable<Page> getSiblings();
+
+    /**
+     * Returns a list of all child pages in the content hierarchy.
+     * @return an iterable list of pages
+     */
+    Iterable<Page> getChildren();
+}
