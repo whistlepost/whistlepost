70985c15676e894d052bf5b990a41f82056b81a7
Ben Fortuna <benfortuna@gmail.com>
2022-04-19 18:26:23 +1000

    Updated docs

--- whistlepost-core/src/main/java/org/whistlepost/model/Layout.java
+++ whistlepost-core/src/main/java/org/whistlepost/model/Layout.java
@@ -6,18 +6,38 @@ import org.apache.sling.models.annotations.Model;
 
 import javax.inject.Inject;
 
+/**
+ * Defines the resources that make up a site layout. Adopting the typical "Holy Grail" layout of websites means
+ * including header and footer sections, an optional sidebar and navigation elements.
+ */
 @Model(adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public interface Layout extends Page {
 
+    /**
+     * Resource path defining the site header configuration.
+     * @return a relative path used for resource resolution
+     */
     @Inject
     String getHeader();
 
+    /**
+     * Resource path defining the site footer configuration.
+     * @return a relative path used for resource resolution
+     */
     @Inject
     String getFooter();
 
+    /**
+     * Resource path defining the site sidebar configuration.
+     * @return a relative path used for resource resolution
+     */
     @Inject
     String getSidebar();
 
+    /**
+     * Resource path defining the site navigation configuration.
+     * @return a relative path used for resource resolution
+     */
     @Inject
     String getNavigation();
 }
--- whistlepost-core/src/main/java/org/whistlepost/model/Legal.java
+++ whistlepost-core/src/main/java/org/whistlepost/model/Legal.java
@@ -6,18 +6,37 @@ import org.apache.sling.models.annotations.Model;
 
 import javax.inject.Inject;
 
+/**
+ * Defines legal terms and conditions for a website.
+ */
 @Model(adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public interface Legal {
 
+    /**
+     * The copyright text typically displayed in the website footer section.
+     * @return a string of copyright text
+     */
     @Inject
     String getCopyright();
 
+    /**
+     * The default license granted for website content.
+     * @return a resource path to a license configuration
+     */
     @Inject
     String getLicense();
 
+    /**
+     * The privacy policy for a website.
+     * @return a resource path to the website privacy policy
+     */
     @Inject
     String getPrivacyPolicy();
 
+    /**
+     * The terms of service for a website.
+     * @return a resource path to the website terms of service.
+     */
     @Inject
     String getTermsOfService();
 }
