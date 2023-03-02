2a5824befc78453265a1cb9ff1515bc8804fbb41
Ben Fortuna <fortuna@micronode.com>
2023-03-02 22:57:29 +1100

    Refactoring to separate repositories

--- whistlepost-config/src/main/java/org/whistlepost/config/form/RadioConfig.java
+++
@@ -1,9 +0,0 @@
-package org.whistlepost.config.form;
-
-import org.apache.sling.caconfig.annotation.Configuration;
-
-@Configuration(name = "radio")
-public @interface RadioConfig {
-
-    String[] values();
-}
